257d52a93b7472953fa123fe0a55c3b53b49acb3
bonugg <gboy77777777@gmail.com>
2023-09-08 10:37:02 +0900

    Feat : 소셜로그인 디다이렉트 주소 수정

--- src/frontEnd/WorldChatProjectReact/srcapp/src/pages/Home.js
+++ src/frontEnd/WorldChatProjectReact/srcapp/src/pages/Home.js
@@ -884,7 +884,7 @@ const Home = React.memo(() => {
             params.append('code', code);
             params.append('client_id', '879795063670-a2a8avf7p2vnlqg9mc526r8ge2h5cgvc.apps.googleusercontent.com');
             params.append('client_secret', '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');
-            params.append('redirect_uri', 'https://www.worldwidechat.co.kr:9002');
+            params.append('redirect_uri', 'https://www.worldwidechat.co.kr:9001');
             params.append('grant_type', 'authorization_code');
 
 
